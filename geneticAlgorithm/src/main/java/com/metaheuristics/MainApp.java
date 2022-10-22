cc62ec8e03c90a744f0dc7105692fc7242cba4ed
Damian2057 <ddamian2057@gmail.com>
2022-10-22 13:41:51 +0200

    adding architecture to simulate generations

--- geneticAlgorithm/src/main/java/com/metaheuristics/MainApp.java
+++ geneticAlgorithm/src/main/java/com/metaheuristics/MainApp.java
@@ -1,11 +1,13 @@
 package com.metaheuristics;
 
 
-import com.metaheuristics.readers.json.JsonReader;
+import com.metaheuristics.simulation.algorithm.GeneticAlgorithm;
+import com.metaheuristics.simulation.algorithm.GeneticAlgorithmImpl;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        System.out.println(JsonReader.getCrossOverProperties().toString());
+        GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithmImpl();
+        geneticAlgorithm.startSimulation();
     }
 }
---
+++ geneticAlgorithm/src/main/java/com/metaheuristics/simulation/algorithm/GeneticAlgorithmImpl.java
@@ -0,0 +1,9 @@
+package com.metaheuristics.simulation.algorithm;
+
+public class GeneticAlgorithmImpl implements GeneticAlgorithm {
+
+    @Override
+    public void startSimulation() {
+
+    }
+}
