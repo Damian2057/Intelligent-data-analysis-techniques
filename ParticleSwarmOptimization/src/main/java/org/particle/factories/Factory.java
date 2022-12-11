84e52cd9d237d29739651dbc18ce4b2356c839f2
Damian2057 <ddamian2057@gmail.com>
2022-12-11 14:15:18 +0100

    adding the basic parameters of the algorithm

--- ParticleSwarmOptimization/src/main/java/org/particle/factories/Factory.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/factories/Factory.java
@@ -1,4 +1,22 @@
 package org.particle.factories;
 
+import org.particle.config.Config;
+import org.particle.model.Particle;
+import org.particle.model.Properties;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Factory {
+
+    private static final Properties prop = Config.getProperties();
+
+    public List<Particle> createSwarm(int size) {
+        List<Particle> swarm = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            swarm.add(new Particle());
+        }
+        return swarm;
+    }
+
 }
--- ParticleSwarmOptimization/src/main/java/org/particle/model/Properties.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/model/Properties.java
@@ -4,5 +4,34 @@ import lombok.Data;
 
 @Data
 public class Properties {
+    private int numberOfParticles;
+    private String xBorder;
+    private String yBorder;
+    private double inertia;
+    private double cognitiveConstant;
+    private double socialConstant;
+    private String adaptationFunction;
+    private int numberOfIteration;
+    private int display;
+    private int startTimes;
 
+    public double getXDownBorder() {
+        String[] border = xBorder.split(",");
+        return Double.parseDouble(border[0]);
+    }
+
+    public double getXUpBorder() {
+        String[] border = xBorder.split(",");
+        return Double.parseDouble(border[1]);
+    }
+
+    public double getYDownBorder() {
+        String[] border = yBorder.split(",");
+        return Double.parseDouble(border[0]);
+    }
+
+    public double getYUpBorder() {
+        String[] border = yBorder.split(",");
+        return Double.parseDouble(border[1]);
+    }
 }
