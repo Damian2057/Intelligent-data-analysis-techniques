65818c7ac4f36e80793722fb13488ff0686469ba
Damian2057 <ddamian2057@gmail.com>
2022-12-11 14:58:49 +0100

    adding dependencies to exp4j to dynamically serve adaption functions

--- ParticleSwarmOptimization/src/main/java/org/particle/model/Particle.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/model/Particle.java
@@ -3,7 +3,7 @@ package org.particle.model;
 public class Particle {
     private double x;
     private double y;
-    private double speed = 0;
+    private double speed;
     private double currentAdaptation;
     private double bestX;
     private double bestY;
@@ -12,6 +12,9 @@ public class Particle {
     public Particle(double x, double y) {
         this.x = x;
         this.y = y;
+        this.speed = 0;
+        this.bestX = x;
+        this.bestY = y;
     }
 
 }
--- ParticleSwarmOptimization/src/main/java/org/particle/simulation/AlgorithmImpl.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/simulation/AlgorithmImpl.java
@@ -1,15 +1,41 @@
 package org.particle.simulation;
 
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 import org.particle.config.Config;
+import org.particle.factories.Factory;
+import org.particle.model.Particle;
 import org.particle.model.Properties;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 public class AlgorithmImpl implements Algorithm {
 
     private static final Properties properties = Config.getProperties();
+    private final static int numberOfIterations = Config.getProperties().getNumberOfIteration();
+    private final Expression expression = new ExpressionBuilder(properties.getAdaptationFunction())
+            .variables("x", "y")
+            .build();
+    private final Logger logger = Logger.getLogger(Algorithm.class.getSimpleName());
+    private final Factory factory = new Factory();
+    private List<Particle> swarm;
+
+
+    public AlgorithmImpl() {
+        logger.info("The simulation has started");
+        swarm = factory.createSwarm(properties.getNumberOfParticles());
+    }
 
     @Override
     public void run() {
+         for (int i = 0; i < numberOfIterations; i++) {
 
+        }
+    }
+
+    private double calculateAdaptation(double x, double y) {
+        return expression.setVariable("x", x).setVariable("y", y).evaluate();
     }
 
 }
