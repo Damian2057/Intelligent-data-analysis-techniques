c7791f3110f57a8c4abc0507dc3f6adf8c3c9401
Damian2057 <ddamian2057@gmail.com>
2022-12-11 19:48:43 +0100

    adding running multiple threads to calculations

--- ParticleSwarmOptimization/src/main/java/org/particle/MainApp.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/MainApp.java
@@ -1,9 +1,16 @@
 package org.particle;
 
+import org.particle.config.Config;
+import org.particle.simulation.Algorithm;
 import org.particle.simulation.AlgorithmImpl;
 
 public class MainApp {
+
+    private static final int startTimes = Config.getProperties().getStartTimes();
+
     public static void main(String[] args) {
-        new Thread(new AlgorithmImpl()).start();
+        for (int i = 0; i < startTimes; i++) {
+            new Thread(new AlgorithmImpl()).start();
+        }
     }
 }
