6144ef1e8412fc423fabecea4a98225f358485d4
Damian2057 <ddamian2057@gmail.com>
2022-12-11 14:22:22 +0100

    creating a Particle factory, along with drawing a starting position from the range

--- ParticleSwarmOptimization/src/main/java/org/particle/factories/Factory.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/factories/Factory.java
@@ -6,6 +6,7 @@ import org.particle.model.Properties;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Factory {
 
@@ -14,9 +15,15 @@ public class Factory {
     public List<Particle> createSwarm(int size) {
         List<Particle> swarm = new ArrayList<>();
         for (int i = 0; i < size; i++) {
-            swarm.add(new Particle());
+            swarm.add(new Particle(drawValue(prop.getXDownBorder(), prop.getXUpBorder()),
+                    drawValue(prop.getYDownBorder(), prop.getYUpBorder())));
         }
         return swarm;
+    }
+
+    private double drawValue(double downRange, double upRange) {
+        Random random = new Random();
+        return downRange + (upRange - downRange) * random.nextDouble();
     }
 
 }
--- ParticleSwarmOptimization/src/main/java/org/particle/model/Particle.java
+++ ParticleSwarmOptimization/src/main/java/org/particle/model/Particle.java
@@ -3,9 +3,15 @@ package org.particle.model;
 public class Particle {
     private double x;
     private double y;
-    private double speed;
+    private double speed = 0;
     private double currentAdaptation;
     private double bestX;
     private double bestY;
     private double bestAdaptation;
+
+    public Particle(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
 }
