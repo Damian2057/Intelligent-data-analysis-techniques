2aa87d9c3ed9b83b4151c37c2e6d1735bc92e2a4
Damian2057 <ddamian2057@gmail.com>
2023-03-14 16:38:12 +0100

    configuration for DE and initial classes to handle the algorithm

--- PSO&DE/src/main/java/p/lodz/pl/config/DEProperties.java
+++ PSO&DE/src/main/java/p/lodz/pl/config/DEProperties.java
@@ -1,11 +1,18 @@
 package p.lodz.pl.config;
 
 import lombok.Data;
-import p.lodz.pl.enums.StopCondition;
+import p.lodz.pl.config.DE.CrossOver;
 
 @Data
 public class DEProperties {
     private final int adaptationFunction;
-    private final StopCondition stopCondition;
+    private final String stopCondition;
     private final double number;
+    private final double[] xRange;
+    private final int dimension;
+
+    private final String reproductionType;
+    private final int populationSize;
+    private final double amplificationFactor;
+    private final CrossOver crossOver;
 }
