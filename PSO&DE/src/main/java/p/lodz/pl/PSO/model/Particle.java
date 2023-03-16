079d2eaf2c16e77bac8eb86ed707867413480458
Damian2057 <ddamian2057@gmail.com>
2023-03-16 21:23:43 +0100

    further work

--- PSO&DE/src/main/java/p/lodz/pl/PSO/model/Particle.java
+++ PSO&DE/src/main/java/p/lodz/pl/PSO/model/Particle.java
@@ -3,42 +3,32 @@ package p.lodz.pl.PSO.model;
 import lombok.Data;
 import p.lodz.pl.Adaptation;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class Particle implements Adaptation, Cloneable {
+public class Particle implements Adaptation {
     private List<Double> xVector;
-    private List<Double> speed;
+    private double speed;
     private double adaptationValue = Double.MAX_VALUE;
     private List<Double> bestXVector;
-    private double bestAdaptation = Double.MAX_VALUE;
+    private double bestAdaptation;
 
-    public Particle(List<Double> x, List<Double> speed) {
+    public Particle(List<Double> x) {
         this.xVector = x;
-        this.speed = speed;
+        this.speed = 0;
         this.bestXVector = x;
     }
 
-    public void setBestAdaptation(double adaptation) {
-        if (adaptation < this.bestAdaptation) {
-            this.bestAdaptation = adaptation;
-            this.bestXVector = new ArrayList<>(xVector);
-        }
+    @Override
+    public String toString() {
+        return "Particle{" +
+                "bestX=" + bestXVector +
+                ", bestAdaptation=" + bestAdaptation +
+                '}';
     }
 
-    @Override
-    public Particle clone() {
-        try {
-            Particle clone = (Particle) super.clone();
-            clone.setSpeed(new ArrayList<>(speed));
-            clone.setBestAdaptation(bestAdaptation);
-            clone.setAdaptationValue(adaptationValue);
-            clone.setXVector(new ArrayList<>(xVector));
-            clone.setBestXVector(new ArrayList<>(bestXVector));
-            return clone;
-        } catch (CloneNotSupportedException e) {
-            throw new AssertionError();
-        }
+    public double setAdaptationValue(double adaptationValue) {
+        this.adaptationValue = adaptationValue;
+        return adaptationValue;
     }
 }
