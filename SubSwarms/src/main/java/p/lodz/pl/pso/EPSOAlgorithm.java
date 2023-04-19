07f0ee15e93fdd85548cbbcfdadf21e51cd6fe6f
Damian2057 <ddamian2057@gmail.com>
2023-04-19 11:19:58 +0200

    preparation of EPSO and OPSO for implementation

--- SubSwarms/src/main/java/p/lodz/pl/pso/EPSOAlgorithm.java
+++ SubSwarms/src/main/java/p/lodz/pl/pso/EPSOAlgorithm.java
@@ -1,12 +1,30 @@
 package p.lodz.pl.pso;
 
+import lombok.extern.java.Log;
 import p.lodz.pl.chart.DataSet;
+import p.lodz.pl.pso.factory.ParticleFactory;
 import p.lodz.pl.pso.model.Particle;
 
 import java.util.List;
 import java.util.concurrent.Future;
 
+@Log
 public class EPSOAlgorithm extends BaseParams implements PSO {
+
+    public EPSOAlgorithm() {
+        super();
+        ParticleFactory factory = new ParticleFactory();
+
+    }
+
+    @Override
+    public Future<PSO> start() {
+        return executor.submit(() -> {
+            log.info(String.format(ALG_START, Thread.currentThread().getName()));
+            return this;
+        });
+    }
+
     @Override
     public List<DataSet> getDataSets() {
         return null;
@@ -14,11 +32,6 @@ public class EPSOAlgorithm extends BaseParams implements PSO {
 
     @Override
     public Particle getBest() {
-        return null;
-    }
-
-    @Override
-    public Future<PSO> start() {
         return null;
     }
 }
