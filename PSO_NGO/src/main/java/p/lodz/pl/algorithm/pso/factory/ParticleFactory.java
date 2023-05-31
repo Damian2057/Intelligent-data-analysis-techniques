b323328298f12a8e4c1ec1fa18cb3705dae9f606
Damian2057 <ddamian2057@gmail.com>
2023-05-31 11:24:43 +0200

    pso impl

---
+++ PSO_NGO/src/main/java/p/lodz/pl/algorithm/pso/factory/ParticleFactory.java
@@ -0,0 +1,40 @@
+package p.lodz.pl.algorithm.pso.factory;
+
+import p.lodz.pl.algorithm.pso.model.Particle;
+import p.lodz.pl.config.Config;
+import p.lodz.pl.config.Properties;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ParticleFactory {
+
+    private static final Properties properties = Config.getProperties();
+
+    public List<Particle> createSwarm(int size) {
+        List<Particle> swarm = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            swarm.add(new Particle(drawValue(properties.getXRange()[0], properties.getXRange()[1]), speedList()));
+        }
+        return swarm;
+    }
+
+    private List<Double> drawValue(double downRange, double upRange) {
+        Random random = new Random();
+        int size = properties.getDimension();
+        List<Double> values = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            double value = downRange + (upRange - downRange) * random.nextDouble();
+            values.add(value);
+        }
+        return values;
+    }
+
+    private List<Double> speedList() {
+        int size = properties.getDimension();
+        return new ArrayList<>(Collections.nCopies(size, 0.0));
+    }
+
+}
