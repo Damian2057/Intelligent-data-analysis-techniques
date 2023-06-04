b93c7cb40429f90a4422bf539060a52e0dfa8540
Damian2057 <ddamian2057@gmail.com>
2023-06-04 21:28:26 +0200

    initial NGO implementation with strategy

--- PSO_NGO/src/main/java/p/lodz/pl/algorithm/ngo/NGOAlgorithm.java
+++ PSO_NGO/src/main/java/p/lodz/pl/algorithm/ngo/NGOAlgorithm.java
@@ -8,7 +8,10 @@ import p.lodz.pl.algorithm.ngo.factory.GoShawkFactory;
 import p.lodz.pl.algorithm.ngo.model.GoShawk;
 import p.lodz.pl.chart.DataSet;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.concurrent.Future;
 
 import static p.lodz.pl.enums.Const.ACCURACY;
@@ -34,8 +37,34 @@ public class NGOAlgorithm extends AlgorithmBase implements NGO {
             log.info(String.format(ALG_START, Thread.currentThread().getName()));
 
             if (ITERATION.getName().equals(properties.getStopCondition())) {
+                for (int i = 0; i < properties.getNumber(); i++) {
+                    bestSolution = getTheBestParticle().clone();
+
+
+
+                    dataSets.add(new DataSet(i, getAvgAdaptation(), bestSolution.getBestAdaptation()));
+                }
 
             } else if (ACCURACY.getName().equals(properties.getStopCondition())) {
+                int repetitionCounter = 0;
+                int index = 0;
+                double repetition = properties.getSize() * 0.5;
+
+                while (repetitionCounter < repetition) {
+                    calculateAdaptation();
+                    bestSolution = getTheBestParticle().clone();
+                    double best = bestSolution.getBestAdaptation();
+
+
+
+                    dataSets.add(new DataSet(index, getAvgAdaptation(), bestSolution.getBestAdaptation()));
+                    if (best - bestSolution.getBestAdaptation() < properties.getNumber()) {
+                        repetitionCounter++;
+                    } else {
+                        repetitionCounter = 0;
+                    }
+                    index++;
+                }
 
             } else {
                 throw new IllegalArgumentException("invalid stop condition of the algorithm");
@@ -59,6 +88,42 @@ public class NGOAlgorithm extends AlgorithmBase implements NGO {
         return bestSolution;
     }
 
+    private GoShawk getTheBestParticle() {
+        return goShawks.stream()
+                .min(Comparator.comparingDouble(GoShawk::getBestAdaptation))
+                .orElseThrow(NoSuchElementException::new);
+    }
+
     private void calculateAdaptation() {
+        goShawks.forEach(particle -> particle.setAdaptationValue(function.function(particle.getXVector())));
+        goShawks.forEach(particle -> particle.setBestAdaptation(Math.min(particle.getAdaptationValue(), particle.getBestAdaptation())));
+    }
+
+    private double getAvgAdaptation() {
+        return goShawks.stream()
+                .mapToDouble(GoShawk::getBestAdaptation)
+                .average()
+                .orElse(0.0);
+    }
+
+    private GoShawk cross(GoShawk current, GoShawk best, int barrier) {
+        List<Double> x = new ArrayList<>();
+        x.addAll(getRange(current.getXVector(), 0, barrier));
+        x.addAll(getRange(best.getXVector(), barrier, properties.getDimension()));
+
+        return new GoShawk(x);
+    }
+
+    private List<Double> getRange(List<Double> list, int start, int end) {
+        List<Double> newList = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            newList.add(list.get(i));
+        }
+
+        return newList;
+    }
+
+    private int random(int min) {
+        return random.nextInt(properties.getDimension() - min) + min;
     }
 }
